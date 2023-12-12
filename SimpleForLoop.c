345ec9e7fa53c83767aa89d85a5961f8cd68f106
Muhammet Hasan Bahar <121157931+mhasanbahar@users.noreply.github.com>
2023-12-12 16:27:43 +0300

    Printing numbers from 1 to 100 on the screen

---
+++ SimpleForLoop.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+//We printed from 1 to 100 using the for loop.
+
+int main(){
+	int i;
+	for(i=1; i<=100; i++)
+	{
+		printf("Number....:%d\n",i);//To show the values ​​obtained at the time the program runs
+	}
+return 0;		
+}
+
